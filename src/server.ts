30922601ad08f74145af917871580aad3d53b8d3
Litzz03 <91480903+Litzz03@users.noreply.github.com>
2024-02-26 20:02:19 -0300

    Aula 26 Fevereiro - Prisma Init

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
 import { fastify } from "fastify";
+import { z } from "zod";
 
 const app = fastify()
 
 app.get('/unipar', () => {
     return 'Ola FASTIFY'
+})
+
+app.post('/uniparpost', (request) => {
+
+const requestBody = z.object(
+    {
+        nome : z.string().optional(),
+        sobrenome : z.string(),
+        idade : z.number()
+    }
+)
+const pessoa = requestBody.parse(request.body)
+
+return pessoa
 })
 
 app.listen({port: 3333}).then( () => {
