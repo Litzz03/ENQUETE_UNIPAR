d6433d770aba18b595ae2b6c2daf912a72d1ad21
Litzz03 <91480903+Litzz03@users.noreply.github.com>
2024-03-19 19:20:16 -0300

    Modificações da Aula do dia 18 de Março 2024

--- src/http/server.ts
+++ src/http/server.ts
@@ -6,19 +6,17 @@ import { prisma } from "../lib/prisma";
 import { criarEnquete } from "./routes/criar-enquete";
 import { listarEnquete } from "./routes/listar-enquete";
 import { obterEnquete } from "./routes/obter-enquete";
+import { votarEnquete } from "./routes/votar-enquete";
 
 const app = fastify()
-
-
-/*app.get('/unipar', () => {
-    return 'Ola FASTIFY'
-})*/
 
 app.register(criarEnquete)
 
 app.register(listarEnquete)
 
 app.register(obterEnquete)
+
+app.register(votarEnquete)
 
 app.listen({port: 3333}).then( () => {
     console.log('SERVIDOR RODANDO')
